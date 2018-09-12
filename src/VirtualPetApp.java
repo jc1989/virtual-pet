1656b5c1e2635c090530d3b8ed6bd42921d0eace
jc1989 <43215306+jc1989@users.noreply.github.com>
2018-09-12 15:10:11 -0500

    Second Commit

--- src/VirtualPetApp.java
+++ src/VirtualPetApp.java
@@ -6,56 +6,65 @@ public static void main(String []args) {
 	Animal loki = new Animal(3,0,"Loki");
 	Animal chopper = new Animal();
 	chopper.setName("Chopper");
-	loki.eat();
-	loki.getHunger();
-	chopper.eat();
-	chopper.eat();
-	chopper.eat();
-	chopper.eat();
-	chopper.eat();
-	chopper.eat();
+//	loki.eat();
+//	loki.getHunger();
+//	chopper.eat();
+//	chopper.eat();
+//	chopper.eat();
+//	chopper.eat();
+//	chopper.eat();
+//	chopper.eat();
 	
-	VirtualPet billy = new VirtualPet();
-	billy.setName("Billy");
-	billy.eat();
-	billy.getHunger();
+//	VirtualPet billy = new VirtualPet();
+//	billy.setName("Billy");
+//	billy.eat();
+//	billy.getHunger();
 	
-	
-	
-	
+	VirtualPet pet = new VirtualPet();
 	
 	
 	
 	
 	
-//	
-//	boolean playing = true;
-//	while (playing == true) {
-//		System.out.println("Choose a Character Name!");
-//		System.out.println("What kind of pet do you have?");
-//		System.out.println("How old is your pet?");
-//		System.out.println("What color if your pet?");
-//		
-//		
-//		
-//		
-//		
-//		
-//		
-//		
-//		
-//		System.out.println("Do you want to continue?");
-//		String userContinue = s.nextLine();
-//		while (!userContinue.equals("no") || !userContinue.equals("yes")) {
-//			playing = false;
-//			System.out.println("Do you want to continue?");
-//			userContinue = s.nextLine();
-//		}
-//		if (userContinue.equals("no")) {
-//			System.exit(0);
-//			
-//		}
-//	}
+	
+	
+	
+	boolean playing = true;
+	while (playing == true) {
+		System.out.println("Choose a Character Name!");
+		String pett = s.nextLine();
+		System.out.println("What kind of pet do you have?");
+		String kind = s.nextLine();
+		System.out.println("How old is your pet?");
+		int age = s.nextInt();
+		s.nextLine();
+		System.out.println("What color is your pet?");
+		String color = s.nextLine();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		System.out.println("Do you want to continue?");
+		String userContinue = s.nextLine();
+		while (!userContinue.equals("no") && !userContinue.equals("yes")) {
+			
+			System.out.println("Do you want to continue? Type Yes, or No");
+			userContinue = s.nextLine();
+			if (userContinue.equals("no")) {
+				System.exit(0);
+				
+			}
+		}
+		if (userContinue.equals("no")) {
+			System.exit(0);
+			
+		}
+	}
 	
 
 }
